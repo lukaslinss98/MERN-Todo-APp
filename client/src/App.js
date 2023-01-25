fd6257c428649094243eb5ca27a59b72e991f323
lukas <lukaslinss98@gmail.com>
2023-01-25 20:43:10 +0100

    added button submit action and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,39 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import './App.css';
 
 function App() {
 
-const [todos, setTodos] = useState({})
+const input = useRef()
+const [todos, setTodos] = useState([])
 
 useEffect(() => {
-  fetch('http://localhost:5000/api')
+  fetch('http://localhost:8080/api')
   .then(
     data => data.json()
     )
   .then(
-      data => setTodos(data)
+      data => setTodos(data.items)
     )
     },[])
+
+  const addTodoHandler = e => {
+    if(e.key === 'Enter'){
+      console.log("Click")  
+      setTodos([...todos, input.current.value])
+    }
+  }
 
   return (
     <div className="App">
      <h1>Your Todos</h1>
-     <ul>
-      {(typeof todos.items === 'undefined') ? (<p>Loading...</p>) : 
-      (todos.items.map((todo, index) => {
-          return <li className='list' key={index}>{todo}</li>
+     <ul className='list'>
+      {(todos.length === 0) ? (<p>Start typing</p>) : 
+      (todos.map((todo, index) => {
+          return <li className='list-item' key={index}>{todo}</li>
         })
       )}
      </ul>
+     <input className="input" ref={input} onKeyDown={addTodoHandler} />
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express()
 app.get("/api", (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.json({"items": ["go shopping", "homework", "test out the MERN stack", "Fix this App"] })
+    res.json({"items": [] })
 })
 
-app.listen(5000, () => {console.log('Server is running on port 5000')})
+app.listen(8080, () => console.log("server is running on port 8080"))
